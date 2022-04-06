0df7a5cc41bf50cabbf238f2a5e03d3d67a087bc
tianbiao <45967570+AmushereTB@users.noreply.github.com>
2022-04-07 00:26:00 +0200

    add delete function, change css

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -27,10 +27,17 @@ const ProfilePage = ({loginInfo} : Props) => {
         fetchFavoriteRecipes();
     },[]);
 
+    const deleteRecipe = async (idMeal: string) : Promise<void> => {
+        await fetch(`https://recipe-backend.azurewebsites.net/api/FavoriteRecipes/${idMeal}`, {
+            method: 'DELETE'
+        });
+        await fetchFavoriteRecipes();
+    }
+    
     return (
         <div>
             <h1 className="section-title">Welcome, {loginInfo?.name}</h1>
-            <FavoriteRecipesBoard items={favoriteList} />
+            <FavoriteRecipesBoard items={favoriteList} deleteRecipe={deleteRecipe} />
         </div>
     );
 };
