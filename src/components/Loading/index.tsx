0267c929c70fd5e713e3ba4b409d0c0f6f3dc822
Tianbiao Feng <tianbiaofeng@gmail.com>
2022-04-13 09:38:20 +0200

    add loading, redirect message, increase ux

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+
+const Loading = () => {
+    return (
+        <h1 className="section-title loading_message">
+            Loading...
+        </h1>
+    );
+};
+
+export default Loading;
